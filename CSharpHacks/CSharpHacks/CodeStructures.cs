4396d3992b40f4473f2db4e76cce344952e420d2
Simon Painter <simon.painter@bet365.com>
2019-09-13 13:20:11 +0100

    add first batch of hacks

---
+++ CSharpHacks/CSharpHacks/CodeStructures.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharpHacks
+{
+    public static class CodeStructures
+    {
+        public static TOutput Map<TInput, TOutput>(this TInput @this, Func<TInput, TOutput> converter) =>
+            converter(@this);
+
+    } 
+}
---
+++ CSharpHacks/CSharpHacks/StringHacks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpHacks
+{
+    public static class StringHacks
+    {
+        public static int ToInt(this string @this) =>
+            int.TryParse(@this, out var result)
+                ? result
+                : 0;
+
+        public static double ToDouble(this string @this) =>
+            double.TryParse(@this, out var result)
+                ? result
+                : 0;
+
+        public static decimal ToDecimal(this string @this) =>
+            decimal.TryParse(@this, out var result)
+                ? result
+                : 0;
+
+        public static float ToFloat(this string @this) =>
+            float.TryParse(@this, out var result)
+                ? result
+                : 0;
+    }
+}
