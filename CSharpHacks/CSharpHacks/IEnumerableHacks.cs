f9e18ca22c8e58f9e1ecb71303e7c0e1469d2e4a
Ken Darling <GuruGuy00@Gmail.com>
2019-10-03 16:33:50 -0400

    Updated Project from .Net Standard 1 to .Net Standard 2
    Added IEnumerable Hacks, I mostly use it for converting List<> to DataTables

---
+++ CSharpHacks/CSharpHacks/IEnumerableHacks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace CSharpHacks {
+    public static class IEnumerableHacks {
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> collection, string tableName)
+        {
+            DataTable tbl = ToDataTable(collection);
+            tbl.TableName = tableName;
+            return tbl;
+        }
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> collection)
+        {
+            DataTable dt = new DataTable();
+            Type t = typeof(T);
+            PropertyInfo[] pia = t.GetProperties();
+            object temp;
+            DataRow dr;
+
+            for (int i = 0; i < pia.Length; i++)
+            {
+                dt.Columns.Add(pia[i].Name, Nullable.GetUnderlyingType(pia[i].PropertyType) ?? pia[i].PropertyType);
+                dt.Columns[i].AllowDBNull = true;
+            }
+
+            //Populate the table
+            foreach (T item in collection)
+            {
+                dr = dt.NewRow();
+
+                dr.BeginEdit();
+
+                for (int i = 0; i < pia.Length; i++)
+                {
+                    temp = pia[i].GetValue(item, null);
+                    if (temp == null || (temp.GetType().Name == "Char" && ((char)temp).Equals('\0')))
+                    {
+                        dr[pia[i].Name] = (object)DBNull.Value;
+                    }
+                    else
+                    {
+                        dr[pia[i].Name] = temp;
+                    }
+                }
+
+                dr.EndEdit();
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+
+        //Export Data to Excel
+        //Export Data to CSV
+
+    }
+}
