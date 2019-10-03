b42a8bb2fc0fb21ce436b02eed4a4028cb114d70
HSBallina <hakan.sjo@valtech.se>
2019-10-03 17:21:59 +0200

    Added Uri hack

---
+++ CSharpHacks/CSharpHacks/UriHacks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpHacks
+{
+    public static class UriHacks
+    {
+        public static Uri AddParameter(this Uri url, string name, string value)
+        {
+            var uriBuilder = new UriBuilder(url);
+            var query = uriBuilder.Query;
+            if (query.Length > 0 && query[0] == '?')
+            {
+                uriBuilder.Query = $"{query.Substring(1)}&{name}={value}";
+            }
+            else
+            {
+                uriBuilder.Query = $"{name}={value}";
+            }
+            
+            return uriBuilder.Uri;
+        }
+    }
+}
