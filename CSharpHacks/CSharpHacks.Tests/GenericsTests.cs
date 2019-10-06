377d658c5073d2820d1257025b74344c3be067a0
IRusio <Jakub_Rusek@onet.pl>
2019-10-06 18:42:07 +0200

    added generic hack and tests

---
+++ CSharpHacks/CSharpHacks.Tests/GenericsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CSharpHacks.Tests
+{
+    public class GenericsTests
+    {
+        [Fact]
+        public void object_is_expected_type()
+        {
+            //Arrange
+            var data = new GenericsHacks.ObjectOne();
+            
+            //Act
+            var result = GenericsHacks.LimitTypeOfGeneric<GenericsHacks.ObjectOne>(data.GetType());
+            
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void object_is_not_expected_type()
+        {
+            //Arrange
+            var data = 0;
+
+            //Act
+            var result = GenericsHacks.LimitTypeOfGeneric<int>(data.GetType());
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
