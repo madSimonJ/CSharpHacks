33a2d14ff0e3f192767b5bdee215753d3592e6b7
Jorge Yañez <ofeth@hotmail.com>
2019-10-07 08:46:30 -0600

    Use FluentAssertions API

--- CSharpHacks/CSharpHacks.Tests/TryCatchTests.cs
+++ CSharpHacks/CSharpHacks.Tests/TryCatchTests.cs
@@ -1,28 +1,53 @@
 using System;
 using Xunit;
+using FluentAssertions;
 
 namespace CSharpHacks.Tests 
 {
     public class TryCatchTests
     {
         [Fact]
-        public void TryOrThrow_ThrowsInvalidOperationException()
+        public void TryOrFailFast_PerformsAction()
         {
-            Assert.Throws<InvalidOperationException>(() =>
-                TryCatchHacks.TryOrThrow<InvalidOperationException>(
-                    () => throw new InvalidOperationException()
-                )
+            Action sut = () => TryCatchHacks.TryOrFailFast<InvalidOperationException>(
+                () =>
+                {
+                    var sum = 1 + 1;
+                    sum += sum;
+                }
             );
+
+            sut.Should().NotThrow<InvalidOperationException>();
         }
 
         [Fact]
-        public void TryOrThrow_ThrowsArgumentException()
+        public void TryOrThrow_ThrowsInvalidOperationException()
         {
-            Assert.Throws<ArgumentException>(() =>
-                TryCatchHacks.TryOrThrow<ArgumentException>(
-                    () => throw new ArgumentException()
-                )
+            Action sut = () => TryCatchHacks.TryOrThrow<InvalidOperationException>(
+                () => throw new InvalidOperationException()
             );
+
+            sut.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void TryOrLogToConsole_ThrowsArgumentException_AndHandleItInternally()
+        {
+            Action sut = () => TryCatchHacks.TryOrLogToConsole<ArgumentException>(
+                () => throw new ArgumentException("To Console")
+            );
+
+            sut.Should().NotThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void TryOrLogToDebug_ThrowsArgumentException_AndHandleItInternally()
+        {
+            Action sut = () => TryCatchHacks.TryOrLogToDebug<ArgumentException>(
+                () => throw new ArgumentException("To Debug")
+            );
+
+            sut.Should().NotThrow<ArgumentException>();
         }
     }
 }
