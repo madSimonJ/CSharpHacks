3bab5fb75c3a0c1a47595b5f1a1d08595f4d77e4
Julius Vitkauskas <zadintuvas@gmail.com>
2019-10-25 00:55:45 +0300

    Add Also object extension

---
+++ CSharpHacks/CSharpHacks.Tests/ObjectTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpHacks.Tests
+{
+    public class ObjectTests
+    {
+        [Fact]
+        public void Also_should_run_additional_code()
+        {
+            var result = ("a" + "b")
+                .Also(TestConsole.WriteLine);
+
+            result.Should().Be("ab");
+            TestConsole.TextWritten.Should().Be("ab");
+        }
+
+        private static class TestConsole
+        {
+            public static string TextWritten = "";
+
+            public static void WriteLine(string text)
+            {
+                TextWritten += text;
+            }
+        }
+    }
+}
---
+++ CSharpHacks/CSharpHacks/ObjectHacks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpHacks
+{
+    public static class ObjectHacks
+    {
+        public static T Also<T>(this T @this, Action<T> action)
+        {
+            action(@this);
+            return @this;
+        }
+    }
+}
