5dbc11a6736ae93c2fc9ef3a7c40c0aec05c075a
Julius Vitkauskas <zadintuvas@gmail.com>
2019-10-25 00:55:45 +0300

    Add Also and Let object extensions

---
+++ CSharpHacks/CSharpHacks.Tests/ObjectTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharpHacks.Tests
+{
+    public class ObjectTests
+    {
+        [Fact]
+        public void Let_should_return_converted_object()
+        {
+            var result = 1
+                .Let(x => x.ToString())
+                .Let(x => $"-{x}");
+
+            result.Should().Be("-1");
+        }
+
+        [Fact]
+        public void Also_should_run_additional_code()
+        {
+            var result = ("a" + "b")
+                .Also(TestConsole.WriteLine);
+
+            result.Should().Be("ab");
+            TestConsole.TextWritten.Should().Be("ab");
+        }
+
+        private static class TestConsole
+        {
+            public static string TextWritten = "";
+
+            public static void WriteLine(string text)
+            {
+                TextWritten += text;
+            }
+        }
+    }
+}
---
+++ CSharpHacks/CSharpHacks/ObjectHacks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpHacks
+{
+    public static class ObjectHacks
+    {
+        public static T Also<T>(this T @this, Action<T> action)
+        {
+            action(@this);
+            return @this;
+        }
+
+        public static K Let<T, K>(this T @this, Func<T, K> func)
+        {
+            return func(@this);
+        }
+    }
+}
