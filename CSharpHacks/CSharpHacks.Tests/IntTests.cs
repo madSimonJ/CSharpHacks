001e7f0a8816bbf5ef704eef820b4c2b0befc43f
Sermet <sermetkose@gmail.com>
2020-10-02 23:30:49 +0300

    Add spesific digit of integer #5

---
+++ CSharpHacks/CSharpHacks.Tests/IntTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CSharpHacks.Tests
+{
+    public class IntTests
+    {
+        [Fact]
+        public void ReplaceFirst_replaces_only_the_first_match()
+        {
+            int number = 54674;
+            number.SpesificDigitOfNumber(1).Should().Be(7);
+        }
+    }
+}
